f9c89e41b3c6127cd56c6bd057591692df54f87d
Ashu <aashubashir@gmail.com>
2022-03-19 15:34:39 +0530

    commit all

---
+++ array.prot.includes.js
@@ -0,0 +1,6 @@
+//JS-ES7 Feature
+// includes function
+// returns a true if array contains the value
+let numbers = [1,2,3,4,5,6];
+console.log(numbers.includes(4));
+console.log(numbers.includes(45));
---
+++ exponentiation.js
@@ -0,0 +1,11 @@
+//JS-ES7 Feature
+//Exponentiation operator
+
+
+var a=3 ;
+var b =4;
+var ans = Math.pow(a,b);
+console.log(ans);
+
+
+console.log(a**b);
