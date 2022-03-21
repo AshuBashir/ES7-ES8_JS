4bc6f87cab8e35b583d92b89594375b32f8d98d9
Ashu <aashubashir@gmail.com>
2022-03-21 16:41:16 +0530

    commit all

--- atomics.js
+++ atomics.js
@@ -1,59 +1,59 @@
-// // create a SharedArrayBuffer
-// var buffer = new SharedArrayBuffer(50);
+// create a SharedArrayBuffer
+var buffer = new SharedArrayBuffer(50);
 
-// // creates an unsigned integer 8-bit array
-// var a = new Uint8Array(buffer);
+// creates an unsigned integer 8-bit array
+var a = new Uint8Array(buffer);
 
-// // Initialising element at zeroth position of array with 9 
-// a[0] = 9;
-// console.log(Atomics.load(a, 0)); 
+// Initialising element at zeroth position of array with 9 
+a[0] = 9;
+console.log(Atomics.load(a, 0)); 
 
-// // Displaying the return value of the Atomics.store() method
-// console.log(Atomics.store(a, 0, 3)); 
+// Displaying the return value of the Atomics.store() method
+console.log(Atomics.store(a, 0, 3)); 
 
-// // Displaying the updated SharedArrayBuffer 
-// console.log(Atomics.load(a, 0));
+// Displaying the updated SharedArrayBuffer 
+console.log(Atomics.load(a, 0));     
 
 
-// new code
-const buffer = new SharedArrayBuffer(2048);
+// // new code
+// const buffer = new SharedArrayBuffer(2048);
+// console.log(buffer)
+// const ta = new Uint8Array(buffer);
 
-const ta = new Uint8Array(buffer);
+// ta[0]; // 0
 
-ta[0]; // 0
+// ta[0] = 5; // 5
 
-ta[0] = 5; // 5
+// Atomics.add(ta, 0, 12); // 5
 
-Atomics.add(ta, 0, 12); // 5
+// Atomics.load(ta, 0);	 // 17
 
-Atomics.load(ta, 0);	 // 17
+// Atomics.and(ta, 0, 1); // 17
 
-Atomics.and(ta, 0, 1); // 17
+// Atomics.load(ta, 0); // 1
 
-Atomics.load(ta, 0); // 1
+// Atomics.exchange(ta, 0, 12); // 1
 
-Atomics.exchange(ta, 0, 12); // 1
+// Atomics.load(ta, 0); // 12
 
-Atomics.load(ta, 0); // 12
+// Atomics.compareExchange(ta, 0, 5, 12); // 1
 
-Atomics.compareExchange(ta, 0, 5, 12); // 1
+// Atomics.load(ta, 0); // 1
 
-Atomics.load(ta, 0); // 1
+// Atomics.isLockFree(1); // true
 
-Atomics.isLockFree(1); // true
+// Atomics.isLockFree(2); // true
 
-Atomics.isLockFree(2); // true
+// Atomics.or(ta, 0, 1); // 12
 
-Atomics.or(ta, 0, 1); // 12
+// Atomics.load(ta, 0); // 13
 
-Atomics.load(ta, 0); // 13
+// Atomics.store(ta, 0, 12); // 12
 
-Atomics.store(ta, 0, 12); // 12
+// Atomics.sub(ta, 0, 2); // 12
 
-Atomics.sub(ta, 0, 2); // 12
+// Atomics.load(ta, 0); // 10
 
-Atomics.load(ta, 0); // 10
+// Atomics.xor(ta, 0, 1); // 10
 
-Atomics.xor(ta, 0, 1); // 10
-
-Atomics.load(ta, 0); // 11
+// Atomics.load(ta, 0); // 11
